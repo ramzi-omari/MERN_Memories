1da1846ec04b357053ed40b81dc38b5fa9d6bdb4
ramzi-omari <ramziomari1995@gmail.com>
2021-01-18 23:42:59 +0100

    Connecting to MongoDB set

--- server/index.js
+++ server/index.js
@@ -6,20 +6,25 @@ import cors from "cors";
 // initialize express app
 const app = express();
 
+//general settings
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNEXION_URL =
-  "mongodb+srv://ramzi-omari:ramzi-omari123@cluster0.bdqgw.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const CONNECTION_URL =
+  "mongodb+srv://ramzi-omari:ramzi-omari@cluster0.gwtbe.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
-
+// to connect to db:
 mongoose
-  .connect(CONNEXION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  // listen has two param port and callback fun
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
+    //if connection is successfull
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
 
+// to assure not getting a warning
 mongoose.set("useFindAndModify", false);
