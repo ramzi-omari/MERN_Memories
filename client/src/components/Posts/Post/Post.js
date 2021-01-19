f7b5e6b84204b8fca594c363b0f8350e559ce49c
ramzi-omari <ramziomari1995@gmail.com>
2021-01-19 09:20:28 +0100

    Styling

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,11 +1,15 @@
 import React from "react";
+import useStyles from "./styles";
 
 const Post = () => {
+  const classes = useStyles();
+
   return (
     <div>
       <h1>GPost</h1>
     </div>
   );
 };
+<Post></Post>;
 
 export default Post;
